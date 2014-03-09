44313f07949be8b4657030239cbb67fce34bb1c7
Thomas Tuegel <ttuegel@gmail.com>
2014-03-09 09:01:53 -0500

    fonts: enable subpixel rendering

--- fonts.nix
+++ fonts.nix
@@ -30,7 +30,7 @@
     <fontconfig>
       <match target="font">
         <edit mode="assign" name="hinting"><bool>true</bool></edit>
-        <edit mode="assign" name="hintstyle"><const>hintfull</const></edit>
+        <edit mode="assign" name="hintstyle"><const>hintslight</const></edit>
       </match>
     </fontconfig>
   '';
@@ -47,25 +47,16 @@
     </fontconfig>
   '';
 
+  nixpkgs.config.packageOverrides = pkgs: {
+    freetype_subpixel = pkgs.freetype.override {
+      useEncumberedCode = true;
+      useInfinality = false;
+    };
+  };
+  environment.systemPackages = [ pkgs.freetype_subpixel ];
+
   environment.variables = {
-    INFINALITY_FT_FILTER_PARAMS = "11 22 38 22 11";
-    INFINALITY_FT_STEM_ALIGNMENT_STRENGTH = "25";
-    INFINALITY_FT_STEM_FITTING_STRENGTH = "25";
-    INFINALITY_FT_STEM_SNAPPING_SLIDING_SCALE = "40";
-    INFINALITY_FT_USE_KNOWN_SETTINGS_ON_SELECTED_FONTS = "true";
-    INFINALITY_FT_CHROMEOS_STYLE_SHARPENING_STRENGTH = "0";
-    INFINALITY_FT_WINDOWS_STYLE_SHARPENING_STRENGTH = "5";
-    INFINALITY_FT_AUTOHINT_SNAP_STEM_HEIGHT = "100";
-    INFINALITY_FT_USE_VARIOUS_TWEAKS = "true";
-    INFINALITY_FT_GAMMA_CORRECTION = "0 100";
-    INFINALITY_FT_BRIGHTNESS = "0";
-    INFINALITY_FT_CONTRAST = "0";
-    INFINALITY_FT_GRAYSCALE_FILTER_STRENGTH = "0";
-    INFINALITY_FT_FRINGE_FILTER_STRENGTH = "0";
-    INFINALITY_FT_AUTOHINT_HORIZONTAL_STEM_DARKEN_STRENGTH = "10";
-    INFINALITY_FT_AUTOHINT_VERTICAL_STEM_DARKEN_STRENGTH = "25";
-    INFINALITY_FT_AUTOHINT_INCREASE_GLYPH_HEIGHTS = "true";
-    QT_GRAPHICSSYSTEM = "native";
+    LD_LIBRARY_PATH = [ "${pkgs.freetype_subpixel}/lib" ];
   };
 
   fonts.extraFonts = with pkgs; [

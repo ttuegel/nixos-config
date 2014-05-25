8649e0fd9f7db7c49bec461569250b37b85167d8
Thomas Tuegel <ttuegel@gmail.com>
2014-05-25 14:39:40 -0500

    fontconfig overhaul

--- fonts.nix
+++ fonts.nix
@@ -2,19 +2,91 @@
 
 {
   fonts = {
-    fontconfig = {
-      autohint = false;
-      embeddedBitmaps = false;
-      user = true;
-      hintStyle = "full";
-      subpixelOrder = "rgb";
-      bitmaps = false;
-    };
-
     infinality = {
       enable = true;
-      qtSubpixel = true;
-      substitutions = false;
+      extraConf = ''
+        <!-- Uncomment this to reject all bitmap fonts -->
+        <!-- Make sure to run this as root if having problems:  fc-cache -f -->
+        <selectfont>
+          <rejectfont>
+            <pattern>
+              <patelt name="scalable" >
+                <bool>false</bool>
+              </patelt>
+            </pattern>
+          </rejectfont>
+        </selectfont>
+
+        <!-- Ban Type-1 fonts because they render poorly -->
+        <!-- Comment this out to allow all Type 1 fonts -->
+        <selectfont>
+          <rejectfont>
+            <pattern>
+              <patelt name="fontformat" >
+                <string>Type 1</string>
+              </patelt>
+            </pattern>
+          </rejectfont>
+        </selectfont>
+
+        <!-- Globally use embedded bitmaps in fonts like Calibri? -->
+        <match target="font" >
+          <edit name="embeddedbitmap" mode="assign">
+            <bool>false</bool>
+          </edit>
+        </match>
+
+        <!-- Substitute truetype fonts in place of bitmap ones? -->
+        <match target="pattern" >
+          <edit name="prefer_outline" mode="assign">
+            <bool>true</bool>
+          </edit>
+        </match>
+
+        <!-- Do font substitutions for the set style? -->
+        <!-- NOTE: Custom substitutions in 42-repl-global.conf will still be done -->
+        <!-- NOTE: Corrective substitutions will still be done -->
+        <match target="pattern" >
+          <edit name="do_substitutions" mode="assign">
+            <bool>false</bool>
+          </edit>
+        </match>
+
+        <!-- Make (some) monospace/coding TTF fonts render as bitmaps? -->
+        <!-- courier new, andale mono, monaco, etc. -->
+        <match target="pattern" >
+          <edit name="bitmap_monospace" mode="assign">
+            <bool>false</bool>
+          </edit>
+        </match>
+
+        <!-- Force autohint always -->
+        <!-- Useful for debugging and for free software purists -->
+        <match target="font">
+          <edit name="force_autohint" mode="assign">
+            <bool>false</bool>
+          </edit>
+        </match>
+
+        <!-- Set DPI.  dpi should be set in ~/.Xresources to 96 -->
+        <!-- Setting to 72 here makes the px to pt conversions work better (Chrome) -->
+        <!-- Some may need to set this to 96 though -->
+        <match target="pattern">
+          <edit name="dpi" mode="assign">
+            <double>96</double>
+          </edit>
+        </match>
+
+        <!-- Use Qt subpixel positioning on autohinted fonts? -->
+        <!-- This only applies to Qt and autohinted fonts. Qt determines subpixel positioning based on hintslight vs. hintfull, -->
+        <!--   however infinality patches force slight hinting inside freetype, so this essentially just fakes out Qt. -->
+        <!-- Should only be set to true if you are not doing any stem alignment or fitting in environment variables -->
+        <match target="pattern" >
+          <edit name="qt_use_subpixel_positioning" mode="assign">
+            <bool>true</bool>
+          </edit>
+        </match>
+      '';
     };
   };
 
@@ -27,5 +99,6 @@
     vistafonts
     corefonts
     wqy_microhei
+    lohit-fonts
   ];
 }

84d2cf696c4ccf63bfada7e46409de9414c581cd
Thomas Tuegel <ttuegel@gmail.com>
2014-05-25 09:05:53 -0500

    fonts: remove defaults

--- fonts.nix
+++ fonts.nix
@@ -1,26 +1,14 @@
 { config, pkgs, ... }:
 
 {
-  environment.etc."fonts/conf.d/50-defaults.conf".text = ''
-    <?xml version="1.0"?>
-    <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
-    <fontconfig>
-      <alias><family>sans-serif</family>
-        <prefer><family>DejaVu Sans</family></prefer></alias>
-      <alias><family>serify</family>
-        <prefer><family>DejaVu Serif</family></prefer></alias>
-      <alias><family>monospace</family>
-        <prefer><family>DejaVu Sans Mono</family></prefer></alias>
-    </fontconfig>
-  '';
-
   fonts = {
     fontconfig = {
       autohint = false;
       embeddedBitmaps = false;
-      user = false;
+      user = true;
       hintStyle = "full";
       subpixelOrder = "rgb";
+      bitmaps = false;
     };
 
     infinality = {
@@ -38,5 +26,6 @@
     dejavu_fonts
     vistafonts
     corefonts
+    wqy_microhei
   ];
 }

21865caedbc6b03bef47bdd5877a60ec5724b116
Thomas Tuegel <ttuegel@gmail.com>
2014-09-27 08:27:02 -0500

    common: absorb user config

--- common.nix
+++ common.nix
@@ -32,15 +32,74 @@
     git
 
     nix-binary-cache
+
+    aspell
+    aspellDicts.en
+    autoconf
+    bazaar
+    cloc
+    darcs
+    git
+    gitAndTools.darcsToGit
+    gitAndTools.gitAnnex
+    gnuplot
+    haskellPackages.cabal2nix
+    haskellPackages.cabalInstall
+    haskellPackages.ghcMod
+    haskellPackages.hledger
+    (haskellPackages.hoogleLocal.override {
+      packages = with haskellPackages; [
+        conduit
+        conduitExtra
+        haskellPlatform
+        hmatrix
+        lens
+        monoTraversable
+        resourcet
+        vector
+        vectorAlgorithms
+      ];
+    })
+    htop
+    llvm
+    manpages
+    mosh
+    mr
+    pdftk
+    silver-searcher
+    stdenv
+    stow
+    tmux
+    wget
+
+    chromiumBeta
+    clementine
+    dropbox
+    emacs
+    firefoxWrapper
+    evince
+    kde4_next.kcolorchooser
+    kde4_next.ksnapshot
+    kde4_next.quassel
+    keepassx2
+    keychain
+    inkscape
+    lyx
+    pidgin
+    vim_configurable # not cli because depends on X
+    vlc
+    zotero
   ];
 
-  environment.variables = {
-    NIX_PATH = pkgs.lib.mkOverride 0 [
-      "nixpkgs=/home/ttuegel/.nix-defexpr/nixpkgs"
-      "nixos=/home/ttuegel/.nix-defexpr/nixpkgs/nixos"
-      "nixos-config=/etc/nixos/configuration.nix"
-    ];
-  };
+  environment.variables =
+    let root_channels = "/nix/var/nix/profiles/per-user/root/channels";
+    in {
+      NIX_PATH = pkgs.lib.mkOverride 0 [
+        ("nixpkgs=" + root_channels + "/unstable")
+        ("nixos=" + root_channels + "/unstable/nixos")
+        "nixos-config=/etc/nixos/configuration.nix"
+      ];
+    };
 
   hardware.pulseaudio.enable = true;
 
@@ -106,31 +165,5 @@
       extraGroups = [ "lp" "networkmanager" "vboxusers" "wheel" ];
     };
   };
-
-  system.replaceRuntimeDependencies = with pkgs;
-    let newPatches = [
-          (fetchurl {
-            url = "mirror://gnu/bash/bash-4.2-patches/bash42-048";
-            sha256 = "091xk1ms7ycnczsl3fx461gjhj69j6ycnfijlymwj6mj60ims6km";
-          })
-          (fetchurl {
-            url = "mirror://gnu/bash/bash-4.2-patches/bash42-049";
-            sha256 = "1d2ympd8icz3q3kbsf2d8inzqpv42q1yg12kynf316msiwxdca3z";
-          })
-        ];
-    in [
-      {
-        original = bash;
-        replacement = pkgs.lib.overrideDerivation bash (oldAttrs: {
-          patches = oldAttrs.patches ++ newPatches;
-        });
-      }
-      {
-        original = bashInteractive;
-        replacement = pkgs.lib.overrideDerivation bashInteractive (oldAttrs: {
-          patches = oldAttrs.patches ++ newPatches;
-        });
-      }
-    ];
 
 }

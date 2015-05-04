6377757f590d06e5abc37d04fc52d8398ebd8c09
Thomas Tuegel <ttuegel@gmail.com>
2015-05-04 07:03:07 -0500

    rebuild from staging

--- common.nix
+++ common.nix
@@ -34,7 +34,6 @@
     gimp
     git
     gitAndTools.darcsToGit
-    gitAndTools.gitAnnex
     gnuplot_qt
     haskellngPackages.cabal2nix
     haskellngPackages.cabal-install
@@ -178,7 +177,7 @@
     firefox = {
       enableAdobeFlash = true;
       enableGoogleTalkPlugin = true;
-      jre = true;
+      jre = false;
     };
     pulseaudio = true;
 

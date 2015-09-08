b8e6c481e12b8ba6ed2e9022495dc58342e09517
Thomas Tuegel <ttuegel@gmail.com>
2015-09-08 11:50:41 -0500

    common: update binary caches and keys

--- common.nix
+++ common.nix
@@ -165,9 +165,15 @@
   nix = {
     binaryCaches = [
       "http://cache.nixos.org/"
+      "http://hydra.nixos.org/"
     ];
     trustedBinaryCaches = [
       "http://192.168.0.3:5000/"
+    ];
+    binaryCachePublicKeys = [
+      "tuegel.mooo.com-1:hZ9VCbn2eRfZl3VVYxkFakWH2SSA18vDv87xxT7BKus="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
     ];
   };
 

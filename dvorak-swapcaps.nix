aeadaff6d46ad1472f1af140e228e796f91f61fd
Thomas Tuegel <ttuegel@gmail.com>
2015-09-29 10:54:03 -0500

    dvorak-swapcaps: generate from upstream keymap
    The keymap is updated whenever the kbd package is updated. Thanks to
    @greyson for the suggestion.

--- dvorak-swapcaps.nix
+++ dvorak-swapcaps.nix
@@ -1,5 +1,7 @@
-{ runCommand }:
+{ runCommand, kbd }:
 
 runCommand "dvorak-swapcaps.map.gz" {} ''
-  gzip - <${./dvorak-swapcaps.map} >$out
+  zcat "${kbd}/share/keymaps/i386/dvorak/dvorak.map.gz" \
+      | sed -e 's/Caps_Lock/Control/g' \
+      | gzip -c >$out
 ''

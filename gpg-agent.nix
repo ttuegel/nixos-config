de81173da011bb9793357969e8c8e276f4ed73a0
Thomas Tuegel <ttuegel@gmail.com>
2015-10-26 19:00:08 -0500

    Revert "gpg-agent: use XDG autostart"
    This reverts commit da98056367d86e863ad75dbe885b6c1abdd03e31.

--- gpg-agent.nix
+++ gpg-agent.nix
@@ -1,14 +1,34 @@
 { config, pkgs, ... }:
 
+let
+
+  startGpgAgent = pkgs.writeScript "start-gpg-agent"
+    ''
+        #!/bin/sh
+        . ${config.system.build.setEnvironment}
+        ${pkgs.gnupg21}/bin/gpg-agent \
+            --enable-ssh-support \
+            --pinentry-program ${pkgs.pinentry_qt}/bin/pinentry-qt4 \
+            --daemon
+    '';
+
+in
+
 {
   programs.ssh.startAgent = false;
-
-  environment.etc."xdg/autostart/gpg-agent.desktop".source
-    = pkgs.makeDesktopItem {
-        name = "gpg-agent";
-        exec = "${pkgs.gnupg}/bin/gpg-agent --enable-ssh-support --pinentry-program ${pkgs.pinentry_qt}/bin/pinentry-qt4 --daemon";
+  systemd.user.services = {
+    gpg-agent = {
+      description = "Secret key management for GnuPG";
+      enable = true;
+      serviceConfig = {
+        Type = "forking";
+        ExecStart = "${startGpgAgent}";
+        ExecStop = "${pkgs.procps}/bin/pkill -u %u gpg-agent";
+        Restart = "always";
       };
-
+      wantedBy = [ "default.target" ];
+    };
+  };
   environment.extraInit = ''
     if [ -n "$TTY" -o -n "$DISPLAY" ]; then
         ${pkgs.gnupg21}/bin/gpg-connect-agent updatestartuptty /bye >/dev/null 2>&1

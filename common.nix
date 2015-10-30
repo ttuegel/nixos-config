d18be85697c015408a92338323e338600687c9b5
Thomas Tuegel <ttuegel@gmail.com>
2015-10-30 12:38:52 -0500

    create kde5test user

--- common.nix
+++ common.nix
@@ -190,7 +190,16 @@
       createHome = true;
       shell = "/var/run/current-system/sw/bin/zsh";
       group = "users";
-      extraGroups = [ "lp" "networkmanager" "vboxusers" "wheel" ];
+      extraGroups = [ "lp" "vboxusers" "wheel" ];
+    };
+    kde5test = {
+      uid = 1001;
+      description = "KDE 5 Tester";
+      home = "/home/kde5test";
+      createHome = true;
+      shell = "/var/run/current-system/sw/bin/zsh";
+      group = "users";
+      extraGroups = [ "lp" "vboxusers" "wheel" ];
     };
   };
 }

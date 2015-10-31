6ff08e3812f0d7a3d149afdb5aa0780bd3552371
Thomas Tuegel <ttuegel@gmail.com>
2015-10-31 16:27:35 -0500

    container-ize quassel

--- mugen.nix
+++ mugen.nix
@@ -23,9 +23,10 @@
   boot.kernelModules = [ "kvm-amd" "radeon" ];
   boot.extraModulePackages = [ ];
 
-  environment.systemPackages = [
-    pkgs.hplipWithPlugin
-  ];
+  containers.quassel = {
+    autoStart = true;
+    config = import ./quassel.nix;
+  };
 
   fileSystems."/" =
     { device = "/dev/sda3";
@@ -47,21 +48,14 @@
       allowPing = true;
       allowedTCPPorts = [ 631 5000 8080 ];
     };
-    interfaces = {
-      br0.ip4 = [ { address = "192.168.1.1"; prefixLength = 24; } ];
-    };
-    bridges = {
-      br0 = { interfaces = [ "enp3s0" "tap-quassel" ]; };
-    };
   };
 
   services.dnsmasq = {
     enable = true;
     extraConfig = ''
-      interface=br0
+      interface=enp3s0
       dhcp-range=192.168.1.2,192.168.1.127
       dhcp-host=DEV1B82FE,192.168.1.2
-      dhcp-host=quassel,192.168.1.3
     '';
     resolveLocalQueries = false;
   };
@@ -76,36 +70,6 @@
     build-cores = 0
     gc-keep-derivations = true
   '';
-
-  users.extraUsers.qemu = {
-    group = "users";
-    home = "/var/lib/qemu";
-    createHome = true;
-    isSystemUser = true;
-  };
-  networking.interfaces.tap-quassel = {
-    virtual = true;
-    virtualType = "tap";
-    virtualOwner = "qemu";
-  };
-
-  systemd.services = {
-    qemu-quassel = {
-      description = "QEMU container for Quassel Core";
-      enable = true;
-      serviceConfig = {
-        Type = "simple";
-        ExecStart =
-          let inherit (import <nixos> { configuration = ./qemu/quassel.nix; }) vm;
-          in "${vm}/bin/run-quassel-vm";
-        User = "qemu";
-        TimeoutSec = 180;
-      };
-      restartIfChanged = true;
-      after = [ "network.target" ];
-      wantedBy = [ "default.target" ];
-    };
-  };
 
   services.nix-serve = {
     enable = true;
---
+++ quassel.nix
@@ -0,0 +1,26 @@
+{ config, lib, pkgs, ... }:
+
+{
+
+  imports = [ ./passwords.nix ];
+
+  services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql94;
+  };
+
+  services.quassel = {
+    enable = true;
+    interface = "0.0.0.0";
+  };
+  systemd.services.quassel.after = [ "postgresql.service" ];
+
+  services.openssh.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    openssl
+  ];
+
+  users.mutableUsers = false;
+
+}

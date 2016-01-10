d201ba9a44439f2b8d910b16384281b187d226d8
Thomas Tuegel <ttuegel@gmail.com>
2016-01-10 07:59:18 -0600

    overrides/gnupg: use gnupg21 as default

--- overrides/gnupg/default.nix
+++ overrides/gnupg/default.nix
@@ -1,6 +1,8 @@
 super: self:
 
 {
+  gnupg = self.gnupg21;
+
   gnupg21 = self.lib.overrideDerivation super.gnupg21 (drv: {
     patches = self.copyPathsToStore (self.lib.readPathsFromFile ./. ./series) ++ drv.patches;
   });

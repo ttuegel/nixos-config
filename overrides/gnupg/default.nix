c09f0547fd957802602285b527f50e9529adb680
Thomas Tuegel <ttuegel@gmail.com>
2016-02-14 15:20:52 -0600

    overrides/gnupg: fix patches

--- overrides/gnupg/default.nix
+++ overrides/gnupg/default.nix
@@ -4,6 +4,7 @@ self: super:
   gnupg = self.gnupg21;
 
   gnupg21 = self.lib.overrideDerivation super.gnupg21 (drv: {
-    patches = self.copyPathsToStore (self.lib.readPathsFromFile ./. ./series) ++ drv.patches;
+    patches = self.copyPathsToStore (self.lib.readPathsFromFile ./. ./series)
+              ++ (drv.patches or []);
   });
 }

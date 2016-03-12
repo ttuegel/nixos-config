67254ecb343a01852a7c12476eb85421d75cc11c
Thomas Tuegel <ttuegel@gmail.com>
2016-03-12 14:29:05 -0600

    programs: install cabal-install from upstream/master

--- programs/default.nix
+++ programs/default.nix
@@ -27,7 +27,13 @@
     gnupg
     gnuplot_qt
     haskellPackages.cabal2nix
-    ttuegel.cabal-install
+    ((ttuegel.cabal-install.override {
+        Cabal = ttuegel.Cabal.overrideDerivation (drv: {
+            src = builtins.filterSource ttuegel.omitGit ./cabal/Cabal;
+        });
+    }).overrideDerivation (drv: {
+      src = builtins.filterSource ttuegel.omitGit ./cabal/cabal-install;
+    }))
     haskellPackages.ghcid
     htop
     ledger

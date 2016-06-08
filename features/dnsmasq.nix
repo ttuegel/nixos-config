8a32bc64ddb8d607cb05038fd8f19343c8eb8dfb
Thomas Tuegel <ttuegel@gmail.com>
2016-06-08 06:22:09 -0500

    features/dnsmasq: fix listening interface

--- features/dnsmasq.nix
+++ features/dnsmasq.nix
@@ -15,7 +15,7 @@
   services.dnsmasq = {
     enable = true;
     extraConfig = ''
-      interface=enp3s0
+      interface=enp2s0
       dhcp-range=192.168.1.2,192.168.1.127
       dhcp-host=DEV1B82FE,192.168.1.2
     '';

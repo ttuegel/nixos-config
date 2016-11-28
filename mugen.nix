21fc1384c4471659a29856eff8a8b1f331b683a9
Thomas Tuegel <ttuegel@gmail.com>
2016-11-28 07:06:41 -0600

    mugen: enable features/fstrim

--- mugen.nix
+++ mugen.nix
@@ -7,6 +7,7 @@
     ./features/desktop.nix
     ./features/dvorak-swapcaps
     ./features/ecryptfs.nix
+    ./features/fstrim.nix
     ./features/hplip
     ./features/nix-serve.nix
     ./programs

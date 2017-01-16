12d120509a2b20bb71b7aa01e4c922da210484c0
Thomas Tuegel <ttuegel@gmail.com>
2017-01-16 17:15:48 -0600

    mesa: enable OpenGL 4 support

--- config/nixpkgs/config.nix
+++ config/nixpkgs/config.nix
@@ -23,6 +23,15 @@ config // {
         feast = self.callPackage ../../pkgs/feast {
           openblas = self.openblasCompat;
         };
+
+        # Enable OpenGL 4 support
+        mesa_drivers = self.mesaDarwinOr (
+          let mo = self.mesa_noglu.override {
+            grsecEnabled = config.nixpkgs.config.grsecurity or false;
+            enableTextureFloats = true;
+          };
+          in mo.drivers
+        );
       })
     ];
 }

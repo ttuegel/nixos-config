7f5f82003afeb49d2339e7cdcd22f2fa80b10e30
Thomas Tuegel <ttuegel@mailbox.org>
2017-03-17 18:11:27 -0500

    programs: Add chibi Scheme

--- programs/default.nix
+++ programs/default.nix
@@ -10,54 +10,58 @@
   services.nixosManual.enable = false; # It's always broken anyway.
 
   environment.systemPackages = with pkgs; [
+    # Encryption
     cryptsetup
-
-    androidenv.platformTools # adb and fastboot
-    aspell aspellDicts.en
-    bibutils
-    clang llvm
-    cloc
-    fricas
-    ghostscript
-    git
     gnupg21 pinentry_qt5
+
+    # Languages
+    chibi
+    clang llvm
+    fricas
     haskellPackages.cabal2nix
     haskellPackages.cabal-install
     haskellPackages.ghc
     haskellPackages.ghcid
+    maxima
+    rustNightlyBin.rustc rustNightlyBin.cargo
+
+    # TeX
+    (texlive.combine {
+      inherit (texlive)
+        scheme-full collection-publishers collection-science;
+    })
+
+    # Utilities
+    androidenv.platformTools # adb and fastboot
+    aspell aspellDicts.en
+    bibutils
+    cloc
+    ghostscript
+    git pass vcsh
     htop
     keybase-go
     ledger
     libburn
     manpages
-    maxima
-    mr vcsh
+    msmtp
     nix-prefetch-scripts nix-repl nox
-    isyncUnstable notmuch msmtp
     pandoc
-    pass
     quilt
     ripgrep
-    rsync
-    rustNightlyBin.rustc rustNightlyBin.cargo
-    (texlive.combine {
-      inherit (texlive)
-        scheme-full collection-publishers collection-science;
-    })
     tmux
-    wget
-    youtube-dl
 
+    # KDE
     audaciousQt5
+    ark
+    gwenview
+    kcolorchooser
+    okular
+    spectacle
+
     firefox-bin  # I don't have time to build Firefox
     gimp
     inkscape
     ipe
-    ark
-    gwenview
-    kcolorchooser
-    spectacle
-    okular
     lyx
     pkgsi686Linux.skype
     spotify

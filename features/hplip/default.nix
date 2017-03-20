3428f59708236f52ac8f8d13e9ac46bc08f625ea
Thomas Tuegel <ttuegel@mailbox.org>
2017-03-20 06:37:34 -0500

    features/hplip: Pin to a working version
    master is broken again

--- features/hplip/default.nix
+++ features/hplip/default.nix
@@ -1,13 +1,27 @@
 { config, pkgs, ... }:
 
+let
+
+  channel = pkgs.fetchFromGitHub {
+    owner = "nixos";
+    repo = "nixpkgs-channels";
+    rev = "2839b101f927be5daab7948421de00a6f6c084ae";
+    sha256 = "0a863cc5462gn1vws87d4qn45zk22m64ri1ip67w0b1a9bmymqdh";
+  };
+
+  inherit (import channel { inherit (config.nixpkgs) config; })
+    hplipWithPlugin;
+
+in
+
 {
   environment.systemPackages = [
-    pkgs.hplipWithPlugin
+    hplipWithPlugin
   ];
 
   # HP printer/scanner support
   hardware.sane.enable = true;
-  hardware.sane.extraBackends = [ pkgs.hplipWithPlugin ];
+  hardware.sane.extraBackends = [ hplipWithPlugin ];
   services.printing.enable = true;
-  services.printing.drivers = [ pkgs.hplipWithPlugin ];
+  services.printing.drivers = [ hplipWithPlugin ];
 }

4ac201528d831c8bc0f10dc34cb65a3d1f5a51e6
Thomas Tuegel <ttuegel@mailbox.org>
2017-03-21 15:42:50 -0500

    mini: Use plain xterm instead of Plasma 5

--- mini.nix
+++ mini.nix
@@ -37,7 +37,7 @@
 
   services.xserver.enable = true;
 
-  services.xserver.desktopManager.kde5.enable = true;
+  services.xserver.desktopManager.xterm.enable = true;
 
   services.xserver.displayManager.sddm.enable = true;
 

559d4babfb9ff85e040fc0304ddd0c54ad9d0fce
Thomas Tuegel <ttuegel@mailbox.org>
2017-07-14 15:07:56 -0500

    programs/mathematica: Fix expression hash

--- programs/mathematica.nix
+++ programs/mathematica.nix
@@ -9,7 +9,7 @@ let
     owner = "ttuegel";
     repo = "nixpkgs";
     rev = "9b4ac151a496fc97e7faf0e81cf35b663eb26a07";
-    sha256 = "046bfmaaxhvd5qfg1f92p5n206aq3vjx1bzrw9r3p80j2h70kpi5";
+    sha256 = "1bs35zxzrb7yqxnsc7v2nw4ss6xbwdmfgip2sikskrgsdpcnic8k";
   };
 
   inherit (import channel { inherit (config.nixpkgs) config; }) mathematica;

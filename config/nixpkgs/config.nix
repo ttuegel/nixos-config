dbf3534d28b02038de66a6959b52992875d53975
Thomas Tuegel <ttuegel@mailbox.org>
2017-07-18 06:59:35 -0500

    Use NetworkManager without Gnome

--- config/nixpkgs/config.nix
+++ config/nixpkgs/config.nix
@@ -9,10 +9,19 @@ let
     pulseaudio = true;
   };
 
+  withoutGnome = drv: drv.override { withGnome = false; };
+
 in
 
 config // {
   packageOverrides = super: let self = super.pkgs; in {
+
+    networkmanager_openvpn = withoutGnome super.networkmanager_openvpn;
+    networkmanager_vpnc = withoutGnome super.networkmanager_vpnc;
+    networkmanager_openconnect = withoutGnome super.networkmanager_openconnect;
+    networkmanager_fortisslvpn = withoutGnome super.networkmanager_fortisslvpn;
+    networkmanager_pptp = withoutGnome super.networkmanager_pptp;
+    networkmanager_l2tp = withoutGnome super.networkmanager_l2tp;
 
     gnupg = self.gnupg21;
 

cea939bc8b210634f5551f4bf41d24196a855051
Thomas Tuegel <ttuegel@mailbox.org>
2017-08-29 16:38:03 -0500

    programs/emacs: Add new packages

--- programs/emacs.nix
+++ programs/emacs.nix
@@ -5,28 +5,25 @@ let
   emacs = pkgs.emacsWithPackages (epkgs: with epkgs; [
     use-package diminish bind-key
     rainbow-delimiters smartparens
-    evil-surround evil-indent-textobject evil-cleverparens # vim
-    undo-tree
+    /* Evil */ evil-surround evil-indent-textobject evil-cleverparens avy undo-tree
     helm
-    avy
-    magit git-timemachine
-    auctex helm-bibtex cdlatex # latex
+    /* Git */ magit git-timemachine
+    /* LaTeX */ auctex helm-bibtex cdlatex
     markdown-mode
     flycheck
     pkgs.ledger
     yaml-mode
     company
-    haskell-mode # haskell
-    org
+    /* Haskell */ haskell-mode flycheck-haskell
+    /* Org */ org org-ref
     rust-mode cargo flycheck-rust
-    notmuch # e-mail
-    pkgs.nix nix-buffer
+    /* mail */ notmuch messages-are-flowing
+    /* Nix */ pkgs.nix nix-buffer
     spaceline # modeline beautification
     winum eyebrowse # window management
     auto-compile
-    pkgs.maxima # for imaxima and imath
+    /* Maxima */ pkgs.maxima
     visual-fill-column
-    org-ref
     melpaStablePackages.idris-mode helm-idris
   ]);
 

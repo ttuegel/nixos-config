bdf39c7d1cac1142992d0f599e3db4251de926df
Thomas Tuegel <ttuegel@mailbox.org>
2017-09-25 10:51:04 -0500

    duo: Adjust import paths for new directory structure

--- duo/configuration.nix
+++ duo/configuration.nix
@@ -3,17 +3,17 @@
 {
   imports = [
     <nixos/modules/installer/scan/not-detected.nix>
-    ./config
-    ./features/desktop.nix
-    ./features/dvorak-swapcaps
-    ./features/ecryptfs.nix
-    ./features/fstrim.nix
-    ./features/hplip.nix
-    ./features/synaptics.nix
-    ./programs
-    ./programs/dropbox.nix
-    ./programs/emacs.nix
-    ./programs/mathematica
+    ../config
+    ../features/desktop.nix
+    ../features/dvorak-swapcaps
+    ../features/ecryptfs.nix
+    ../features/fstrim.nix
+    ../features/hplip.nix
+    ../features/synaptics.nix
+    ../programs
+    ../programs/dropbox.nix
+    ../programs/emacs.nix
+    ../programs/mathematica
   ];
 
   boot.initrd.availableKernelModules = [ "ehci_hcd" "ahci" ];

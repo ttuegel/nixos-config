ae1d7d0141a959a9762e36c9c3b3a23864c2297e
Thomas Tuegel <ttuegel@mailbox.org>
2017-12-22 08:44:13 -0600

    programs/emacs: Install projectile

--- programs/emacs.nix
+++ programs/emacs.nix
@@ -3,33 +3,71 @@
 let
 
   emacs = pkgs.emacsWithPackages (epkgs: with epkgs; [
-    use-package diminish bind-key
-    rainbow-delimiters smartparens
-    /* Evil */ evil-surround evil-indent-textobject evil-cleverparens avy undo-tree
+    # Interface
+    bind-key
+    company
     helm
-    /* Git */ magit git-timemachine
-    /* LaTeX */ auctex helm-bibtex cdlatex company-math
-    markdown-mode
+    helm-descbinds  # describe-bindings in helm
+    helm-projectile
+    projectile  # project management
+    visual-fill-column
+    which-key  # display keybindings after incomplete command
+    winum eyebrowse # window management
+
+    # Themes
+    base16-theme
+    diminish
+    spaceline # modeline beautification
+
+    # Delimiters
+    rainbow-delimiters smartparens
+
+    # Evil
+    avy
+    evil-surround
+    evil-indent-textobject
+    evil-cleverparens
+    undo-tree
+
+    # Git
+    magit
+    git-timemachine
+
+    # LaTeX
+    auctex
+    cdlatex
+    company-math
+    helm-bibtex
+
+    auto-compile
     flycheck
+
+    markdown-mode
     pkgs.ledger
     yaml-mode
-    company
-    /* Haskell */ haskell-mode flycheck-haskell
-    /* Org */ org org-ref
-    rust-mode cargo flycheck-rust
-    /* mail */ notmuch messages-are-flowing
-    /* Nix */ pkgs.nix nix-buffer
-    spaceline # modeline beautification
-    winum eyebrowse # window management
-    auto-compile
-    /* Maxima */ pkgs.maxima
-    visual-fill-column
-    melpaStablePackages.idris-mode helm-idris
 
-    which-key  # display keybindings after incomplete command
-    helm-descbinds  # describe-bindings in helm
+    # Haskell
+    haskell-mode
+    flycheck-haskell
     company-ghci  # provide completions from inferior ghci
-    base16-theme
+
+    # Org
+    org org-ref
+
+    # Rust
+    rust-mode cargo flycheck-rust
+
+    # Mail
+    notmuch messages-are-flowing
+
+    # Nix
+    pkgs.nix nix-buffer
+
+    # Maxima
+    pkgs.maxima
+
+    # Idris
+    melpaStablePackages.idris-mode helm-idris
   ]);
 
   autostartEmacsDaemon = pkgs.writeTextFile {

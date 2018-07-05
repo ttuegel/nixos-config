b9a9660bae842cb6aeb0399a7aa9f516a2df2509
Thomas Tuegel <ttuegel@mailbox.org>
2018-07-05 12:56:10 -0500

    programs/haskell: Install stack and stylish-haskell

--- programs/haskell.nix
+++ programs/haskell.nix
@@ -3,6 +3,6 @@
 {
   environment.systemPackages = with pkgs; with haskellPackages; [
     haskell.compiler.ghc822
-    cabal2nix cabal-install hpack
+    cabal2nix cabal-install hpack stack stylish-haskell
   ];
 }

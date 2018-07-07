ebf4c8e34d1b3ff6b9f4c6229f8bab6deed5f199
Thomas Tuegel <ttuegel@mailbox.org>
2018-07-07 10:32:40 -0500

    programs/haskell: install hindent and hlint

--- programs/haskell.nix
+++ programs/haskell.nix
@@ -3,6 +3,12 @@
 {
   environment.systemPackages = with pkgs; with haskellPackages; [
     haskell.compiler.ghc822
-    cabal2nix cabal-install hpack stack stylish-haskell
+    cabal2nix
+    cabal-install
+    hindent
+    hlint
+    hpack
+    stack
+    stylish-haskell
   ];
 }

ff3f6a49d4070599bd114b8f605e6a0c7f329ffe
Thomas Tuegel <ttuegel@mailbox.org>
2018-07-24 05:13:19 -0500

    programs/hie-nix: Use upstream configuration

--- programs/hie-nix/default.nix
+++ programs/hie-nix/default.nix
@@ -3,14 +3,14 @@
 let inherit (pkgs) fetchgit; in
 
 let
-  lock =
-    builtins.fromJSON (builtins.readFile ./hie-nix.json);
-  bootstrap = fetchgit {
-    inherit (lock) url rev sha256 fetchSubmodules;
+  hie-nix = fetchgit {
+    inherit (builtins.fromJSON (builtins.readFile ./hie-nix.json))
+      url rev sha256 fetchSubmodules;
   };
-  hie-nix = import bootstrap { inherit pkgs; };
 in
 
 {
-  environment.systemPackages = [ hie-nix.hies ];
+  environment.systemPackages =
+    with (import hie-nix {});
+    [ hie82 hie84 ];
 }

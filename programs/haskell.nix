dd6edade4468835192788a98e8b44fc60d0cbed3
Thomas Tuegel <ttuegel@mailbox.org>
2018-09-10 08:26:14 -0500

    programs/haskell: Install hie

--- programs/haskell.nix
+++ programs/haskell.nix
@@ -2,11 +2,24 @@
 
 with pkgs; with haskellPackages;
 
+let
+  hie-nix =
+    import (
+      fetchFromGitHub {
+        owner = "domenkozar";
+        repo = "hie-nix";
+        inherit (lib.importJSON ./hie-nix.lock.json)
+          rev sha256;
+      }
+    ) {};
+in
+
 {
   environment.systemPackages = with pkgs; with haskellPackages; [
     ghc
     cabal2nix
     cabal-install
+    hie-nix.hies
     hlint
     hpack
     stack

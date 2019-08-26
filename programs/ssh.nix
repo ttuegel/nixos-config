c1b33cc2d567fa619e7132da3013d2772256af36
Thomas Tuegel <ttuegel@mailbox.org>
2019-08-26 11:06:25 -0500

    programs/ssh: Run OpenSSH on the standard port

--- programs/ssh.nix
+++ programs/ssh.nix
@@ -5,9 +5,5 @@
     enable = true;
     passwordAuthentication = false;
     permitRootLogin = "no";
-    ports = [ 2222 ];
   };
-
-  networking.firewall.allowedTCPPorts = [ 2222 ];
-  networking.firewall.allowedUDPPorts = [ 2222 ];
 }

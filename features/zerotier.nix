8c8b643535e3953d25f1eddada62eea020bcb294
Thomas Tuegel <ttuegel@mailbox.org>
2020-02-11 09:59:21 -0600

    features/zerotier.nix: Add hosts

--- features/zerotier.nix
+++ features/zerotier.nix
@@ -11,8 +11,17 @@ in
   # Advertise mDNS over ZeroTier only.
   services.avahi.interfaces = privateZeroTierInterfaces;
 
+  # Join my private ZeroTier network.
   services.zerotierone = {
     enable = true;
     joinNetworks = [ "af415e486fda8d4b" ];
   };
+
+  # Map ZeroTier addresses to hostnames.
+  networking.hosts = {
+    "10.147.20.98"  = [ "dioscuri" "dioscuri.local" ];
+    "10.147.20.131" = [ "demeter"  "demeter.local"  ];
+    "10.147.20.206" = [ "mugen"    "mugen.local"    ];
+  };
+
 }

fc5b7721569da4ceca87b275119719bf66abf073
Thomas Tuegel <ttuegel@mailbox.org>
2020-12-21 06:21:03 -0600

    zeus/gitolite: Use ZFS

--- hosts/zeus/gitolite.nix
+++ hosts/zeus/gitolite.nix
@@ -1,6 +1,12 @@
 { ... }:
 
 {
+
+  fileSystems."/var/lib/gitolite" = {
+    device = "tank/gitolite";
+    fsType = "zfs";
+  };
+
   services.gitolite = {
     enable = true;
     adminPubkey = builtins.readFile ./gitolite-admin.pub;

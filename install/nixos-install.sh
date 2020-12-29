608549cd5b1b40d984cbde264c8324f1cec010b6
Thomas Tuegel <ttuegel@mailbox.org>
2020-12-29 08:11:28 -0600

    nixos-install.sh: Fix binary-caches option name

--- install/nixos-install.sh
+++ install/nixos-install.sh
@@ -2,6 +2,6 @@
 
 host=$1; shift
 nixos-install \
-    --option binary-cache "$host" \
+    --option binary-caches "https://cache.nixos.org $host" \
     --option trusted-public-keys 'cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= zeus-1:hpocFIqCGUxWFSSlvq5V0ImyCQhl+LcnCB21C7bhqjs=' \
     "$@"

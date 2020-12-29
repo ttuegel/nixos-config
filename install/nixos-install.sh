088baa16228d0ebc9732f81a5d73fa21746c66a3
Thomas Tuegel <ttuegel@mailbox.org>
2020-12-29 14:38:16 -0600

    nixos-install.sh: Use only the local cache

--- install/nixos-install.sh
+++ install/nixos-install.sh
@@ -2,6 +2,6 @@
 
 host=$1; shift
 nixos-install \
-    --option binary-caches "https://cache.nixos.org $host" \
-    --option trusted-public-keys 'cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= zeus-1:hpocFIqCGUxWFSSlvq5V0ImyCQhl+LcnCB21C7bhqjs=' \
+    --option binary-caches "$host" \
+    --option trusted-public-keys 'zeus-1:hpocFIqCGUxWFSSlvq5V0ImyCQhl+LcnCB21C7bhqjs=' \
     "$@"

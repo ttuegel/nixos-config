14bcbbebf1eacd60afdc0b8a915915800535273e
Thomas Tuegel <ttuegel@mailbox.org>
2021-01-04 05:40:01 -0600

    features/haskell.nix: Install haskell-language-server

--- features/haskell.nix
+++ features/haskell.nix
@@ -1,10 +1,19 @@
 { lib, pkgs, ... }:
 
+let
+  sources = import ../nix/sources.nix;
+  hls_8_10_1 = import sources."nix-haskell-hls" { ghcVersion = "ghc8101"; };
+  hls_8_10_2 = import sources."nix-haskell-hls" { ghcVersion = "ghc8102"; };
+in
+
 with pkgs; with haskellPackages;
 
 {
-  environment.systemPackages = with pkgs; with haskellPackages; [
-    ghc
+  environment.systemPackages = [
+    haskell.compiler.ghc8102
+    hls_8_10_2.hls-wrapper
+    hls_8_10_2.hls-renamed
+    hls_8_10_1.hls-renamed
     cabal-install
     ghcid
     hlint

1fc082e66da2c2bac35152283ca54dca42c93285
Thomas Tuegel <ttuegel@mailbox.org>
2021-01-04 11:10:13 -0600

    programs/vscode: Automatically update extensions

---
+++ programs/vscode-exts.nix
@@ -0,0 +1,75 @@
+{ extensions = [
+  {
+    name = "EditorConfig";
+    publisher = "EditorConfig";
+    version = "0.16.4";
+    sha256 = "0fa4h9hk1xq6j3zfxvf483sbb4bd17fjl5cdm3rll7z9kaigdqwg";
+  }
+  {
+    name = "nix-env-selector";
+    publisher = "arrterian";
+    version = "0.1.2";
+    sha256 = "1n5ilw1k29km9b0yzfd32m8gvwa2xhh6156d4dys6l8sbfpp2cv9";
+  }
+  {
+    name = "Nix";
+    publisher = "bbenoist";
+    version = "1.0.1";
+    sha256 = "0zd0n9f5z1f0ckzfjr38xw2zzmcxg1gjrava7yahg5cvdcw6l35b";
+  }
+  {
+    name = "dhall-lang";
+    publisher = "dhall";
+    version = "0.0.4";
+    sha256 = "0sa04srhqmngmw71slnrapi2xay0arj42j4gkan8i11n7bfi1xpf";
+  }
+  {
+    name = "gitlens";
+    publisher = "eamodio";
+    version = "11.1.2";
+    sha256 = "1vfj13bcxcg540cim6jmwz8y674p0z4inzipal8vxa13a8pf8y9r";
+  }
+  {
+    name = "vscode-pull-request-github";
+    publisher = "GitHub";
+    version = "0.22.0";
+    sha256 = "13p3z86vkra26npp5a78pxdwa4z6jqjzsd38arhgdnjgwmi6bnrw";
+  }
+  {
+    name = "haskell";
+    publisher = "haskell";
+    version = "1.2.0";
+    sha256 = "0vxsn4s27n1aqp5pp4cipv804c9cwd7d9677chxl0v18j8bf7zly";
+  }
+  {
+    name = "language-haskell";
+    publisher = "justusadam";
+    version = "3.3.0";
+    sha256 = "1285bs89d7hqn8h8jyxww7712070zw2ccrgy6aswd39arscniffs";
+  }
+  {
+    name = "vscode-docker";
+    publisher = "ms-azuretools";
+    version = "1.9.0";
+    sha256 = "10xih3djdbxvndlz8s98rf635asjx8hmdza49y67v624i59jdn3x";
+  }
+  {
+    name = "remote-ssh";
+    publisher = "ms-vscode-remote";
+    version = "0.62.0";
+    sha256 = "043q3rbhmyx3i5xd0v3yx7hrybmk7mzy8r95c7cf927w2panx59l";
+  }
+  {
+    name = "cpptools";
+    publisher = "ms-vscode";
+    version = "1.1.3";
+    sha256 = "02kx7yv3n22cja8pxnj0i03c4jx4v55iykcgbwxh37vknv8zv376";
+  }
+  {
+    name = "rewrap";
+    publisher = "stkb";
+    version = "1.13.0";
+    sha256 = "18h42vfxngix8x22nqslvnzwfvfq5kl35xs6fldi211dzwhw905j";
+  }
+];
+}

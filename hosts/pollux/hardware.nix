dfbb8cc5fad6a987adc5f0b30be316ec9a3c1e7d
Thomas Tuegel <ttuegel@mailbox.org>
2021-02-20 06:33:16 -0600

    hosts/pollux: Allow Intel microcode updates

--- hosts/pollux/hardware.nix
+++ hosts/pollux/hardware.nix
@@ -31,4 +31,7 @@
   swapDevices = [ ];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+
+  hardware.cpu.intel.updateMicrocode =
+    lib.mkDefault config.hardware.enableRedistributableFirmware;
 }

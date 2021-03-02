f65c5bb02b2f2bf4186d13b98caa9bd27f3fc4a3
Thomas Tuegel <ttuegel@mailbox.org>
2021-03-02 09:53:32 -0600

    Update nix-haskell-hls

--- features/haskell.nix
+++ features/haskell.nix
@@ -2,18 +2,15 @@
 
 let
   sources = import ../nix/sources.nix;
-  hls_8_10_1 = import sources."nix-haskell-hls" { ghcVersion = "ghc8101"; };
-  hls_8_10_2 = import sources."nix-haskell-hls" { ghcVersion = "ghc8102"; };
+  hls_8_10_4 = import sources."nix-haskell-hls" { ghcVersion = "ghc8104"; };
 in
 
 with pkgs; with haskellPackages;
 
 {
   environment.systemPackages = [
-    haskell.compiler.ghc8102
-    hls_8_10_2.hls-wrapper
-    hls_8_10_2.hls-renamed
-    hls_8_10_1.hls-renamed
+    haskell.compiler.ghc8104
+    hls_8_10_4.hls-wrapper
     cabal-install
     ghcid
     hlint

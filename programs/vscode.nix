b56d552b1a4c936d249bad0e738a45dd9f57c9a8
Thomas Tuegel <ttuegel@mailbox.org>
2021-08-06 09:31:16 -0500

    vscode: Update extensions

--- programs/vscode.nix
+++ programs/vscode.nix
@@ -6,29 +6,29 @@ let
   gitlens = vscode-utils.extensionFromVscodeMarketplace {
     name = "gitlens";
     publisher = "eamodio";
-    version = "11.0.6";
-    sha256 = "0qlaq7hn3m73rx9bmbzz3rc7khg0kw948z2j4rd8gdmmryy217yw";
+    version = "11.6.0";
+    sha256 = "sha256:0lhrw24ilncdczh90jnjx71ld3b626xpk8b9qmwgzzhby89qs417";
   };
 
   nix-env-selector = vscode-utils.extensionFromVscodeMarketplace {
     name = "nix-env-selector";
     publisher = "arrterian";
-    version = "0.1.2";
-    sha256 = "1n5ilw1k29km9b0yzfd32m8gvwa2xhh6156d4dys6l8sbfpp2cv9";
+    version = "1.0.7";
+    sha256 = "sha256:0mralimyzhyp4x9q98x3ck64ifbjqdp8cxcami7clvdvkmf8hxhf";
   };
 
   editorconfig = vscode-utils.extensionFromVscodeMarketplace {
     name = "editorconfig";
     publisher = "editorconfig";
     version = "0.16.4";
-    sha256 = "0fa4h9hk1xq6j3zfxvf483sbb4bd17fjl5cdm3rll7z9kaigdqwg";
+    sha256 = "sha256:0fa4h9hk1xq6j3zfxvf483sbb4bd17fjl5cdm3rll7z9kaigdqwg";
   };
 
   rewrap = vscode-utils.extensionFromVscodeMarketplace {
     name = "rewrap";
     publisher = "stkb";
-    version = "1.13.0";
-    sha256 = "18h42vfxngix8x22nqslvnzwfvfq5kl35xs6fldi211dzwhw905j";
+    version = "1.14.0";
+    sha256 = "sha256:0phffzqv1nmwsgcx6abgzbzw95zc0zlnhsjv2grs5mcsgrghl759";
   };
 
   vscode = vscode-with-extensions.override {

8f3c9c205907afc56a5a01b730d56f7585f38a7f
Thomas Tuegel <ttuegel@mailbox.org>
2022-03-09 10:16:45 -0600

    features/zerotier.nix: Update hosts
    Add new hosts, remove old hosts, and add IPv6 addresses.

--- features/zerotier.nix
+++ features/zerotier.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 
 let
   privateZeroTierInterfaces = [ "zt44xijrje" ];
@@ -18,11 +18,19 @@ in
   };
 
   # Map ZeroTier addresses to hostnames.
-  networking.hosts = {
-    "10.147.20.25"  = [ "pollux"   "pollux.local"   ];
-    "10.147.20.98"  = [ "dioscuri" "dioscuri.local" ];
-    "10.147.20.131" = [ "demeter"  "demeter.local"  ];
-    "10.147.20.191" = [ "zeus"     "zeus.local"     ];
-  };
+  networking.hosts =
+    let
+      hosts = {
+        maia   = ["10.147.20.29"  "fdaf:415e:486f:da8d:4b99:9387:e1be:deb3"];
+        hermes = ["10.147.20.102" "fdaf:415e:486f:da8d:4b99:93d9:a407:676c"];
+        pollux = ["10.147.20.25"  "fdaf:415e:486f:da8d:4b99:93e9:0fc5:a780"];
+        zeus   = ["10.147.20.191" "fdaf:415e:486f:da8d:4b99:93ad:ab2c:ef1a"];
+      };
+      hostLine = host: addr: { "${addr}" = [ host "${host}.local" ]; };
+      hostLines = host: addrs: concatAttrs (builtins.map (hostLine host) addrs);
+      concatAttrs1 = attr1: attr2: attr1 // attr2;
+      concatAttrs = builtins.foldl' concatAttrs1 {};
+    in
+      concatAttrs (lib.mapAttrsToList hostLines hosts);
 
 }

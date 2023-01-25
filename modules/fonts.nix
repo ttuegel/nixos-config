9ffa1b0a4d4f26730158804daebd0e26c3566c54
Thomas Tuegel <thomas@mercury.com>
2023-01-25 10:51:59 -0600

    Revert "Use Iosevka OpenType features instead of building from source"
    This reverts commit 21fc9443b97a416ab38a79a81192cc9b86c90724.

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -3,47 +3,13 @@
 {
   fonts.fontconfig = {
     defaultFonts = {
-      monospace = [ "Iosevka" ];
+      monospace = [ "Iosevka Custom" "Hack" "Source Code Pro" "DejaVu Sans Mono" ];
       sansSerif = [ "Noto Sans" ];
       serif = [ "Noto Serif" ];
     };
     allowBitmaps = false;
     allowType1 = false;
     includeUserConf = false;
-
-    localConf = ''
-      <?xml version="1.0"?>
-      <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
-      <fontconfig>
-        <match target="font">
-          <test name="family" compare="contains">
-            <string>Iosevka</string>
-          </test>
-          <edit name="fontfeatures" mode="append">
-            <string>cv32=3</string> <!-- g = single-storey-serifless -->
-            <string>cv34=8</string> <!-- i = tailed-serifed -->
-            <string>cv37=8</string> <!-- l = tailed-serifed -->
-            <string>cv71=4</string> <!-- 0 = dotted -->
-            <string>cv75=1</string> <!-- 4 = closed -->
-            <string>cv82=6</string> <!-- * = hex-low -->
-            <string>cv87=1</string> <!-- { = straight -->
-          </edit>
-        </match>
-
-        <match target="font">
-          <test name="family" compare="contains">
-            <string>Iosevka</string>
-          </test>
-          <test name="slant" compare="eq">
-            <const>italic</const>
-          </test>
-          <edit name="fontfeatures" mode="append">
-            <string>cv36=2</string> <!-- k = curly-serifless -->
-            <string>cv49=2</string> <!-- y = straight-turn -->
-          </edit>
-        </match>
-      </fontconfig>
-    '';
   };
 
   fonts.fonts = with pkgs; with lib;
@@ -55,7 +21,8 @@
       emacs-all-the-icons-fonts
       font-awesome_4
       gentium
-      iosevka-bin
+      iosevka-custom
+      iosevka-custom-terminal
       noto-fonts
       noto-fonts-cjk
       noto-fonts-emoji

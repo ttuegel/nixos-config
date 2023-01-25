2e71940901e46bae9c36b04160058ae4cf17945e
Thomas Tuegel <thomas@mercury.com>
2023-01-25 10:21:14 -0600

    modules/fonts.nix: Clean up font list

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -12,35 +12,23 @@
     includeUserConf = false;
   };
 
-  fonts.fonts = with pkgs; with lib; [
-    cm_unicode
-    corefonts
-    dejavu_fonts
-    eb-garamond
-    emacs-all-the-icons-fonts
-    emojione
-    font-awesome_4
-    gentium
-    iosevka-custom
-    iosevka-custom-terminal
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-    source-code-pro
-    source-sans-pro
-    source-serif-pro
-    tex-gyre.adventor
-    tex-gyre.bonum
-    tex-gyre.chorus
-    tex-gyre.cursor
-    tex-gyre.heros
-    tex-gyre.pagella
-    tex-gyre.schola
-    tex-gyre.termes
-    tex-gyre-math.bonum
-    tex-gyre-math.pagella
-    tex-gyre-math.schola
-    tex-gyre-math.termes
-    vistafonts
-  ];
+  fonts.fonts = with pkgs; with lib;
+    [
+      cm_unicode
+      corefonts
+      dejavu_fonts
+      eb-garamond
+      emacs-all-the-icons-fonts
+      font-awesome_4
+      gentium
+      iosevka-custom
+      iosevka-custom-terminal
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      vistafonts
+    ]
+    ++ lib.attrValues tex-gyre
+    ++ lib.attrValues tex-gyre-math;
+
 }

2d9a3b2bdae63b6b203bc9424904037b1c461051
Thomas Tuegel <thomas@mercury.com>
2023-01-27 10:41:29 -0600

    flake.lock: Use Nixpkgs master for Zoom bug fix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
   inputs.secrets = {
     url = "git+ssh://gitolite@zeus/ttuegel/nixos-secrets?ref=main";
     flake = false;

889da97d13ad85daf4a2d2b69c882982194d9ec3
Thomas Tuegel <ttuegel@mailbox.org>
2023-11-28 19:29:10 -0600

    budgie: Set up object store key

--- hosts/budgie/nextcloud.nix
+++ hosts/budgie/nextcloud.nix
@@ -18,6 +18,12 @@
     group = "nextcloud";
   };
 
+  age.secrets.nextcloud-backblaze-secret = {
+    file = "${secrets}/hosts/budgie/nextcloud-backblaze-secret";
+    owner = "nextcloud";
+    group = "nextcloud";
+  };
+
   services.nextcloud = {
     enable = true;
     package = pkgs.nextcloud27;
@@ -34,8 +40,10 @@
         enable = true;
         hostname = "s3.us-west-004.backblazeb2.com";
         bucket = "cloud-enchanted-earth";
-        autocreate = false;
         usePathStyle = true;
+        autocreate = false;
+        key = "004e252d0df953f0000000002";
+        secretFile = config.age.secrets.nextcloud-backblaze-secret.path;
       };
     };
   };
